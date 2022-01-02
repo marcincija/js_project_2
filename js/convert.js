ba3c2ff22d0b7e73576cfbb5cc0f85be1a6cbb88
Marcin <marcin.cija@gmail.com>
2022-01-02 17:42:08 +0100

    update

--- js/convert.js
+++ js/convert.js
@@ -8,38 +8,43 @@ let currnecy = () => {
     axios({
       method: "get",
       url: "https://api.nbp.pl/api/exchangerates/tables/a/today/",
-    }).then(function (response) {
-      let data = response.data;
-      let rates = data[0].rates;
+    })
+      .then(function (response) {
+        let data = response.data;
+        let rates = data[0].rates;
 
-      let names = rates.map((name) => {
-        return name.code;
-      });
+        let names = rates.map((name) => {
+          return name.code;
+        });
 
-      let currentRates = rates.map((rates) => {
-        return rates.mid;
-      });
+        let currentRates = rates.map((rates) => {
+          return rates.mid;
+        });
 
-      for (let i = 0; i < names.length; i++) {
-        list.innerHTML += `<option>${names[i]}</option>`;
-      }
-
-      console.log(names);
-
-      getNumber = () => {
-        let number = document.getElementById("number").value;
-
-        for (i = 0; i < names.length; ++i) {
-          if (names[id] != getCurrnecyName.value) {
-            id++;
-          }
+        for (let i = 0; i < names.length; i++) {
+          list.innerHTML += `<option>${names[i]}</option>`;
         }
 
-        result.innerHTML = `${number * currentRates[id]}`;
-        id = 0;
-        return result;
-      };
-    });
+        console.log(names);
+
+        getNumber = () => {
+          let number = document.getElementById("number").value;
+
+          for (i = 0; i < names.length; ++i) {
+            if (names[id] != getCurrnecyName.value) {
+              id++;
+            }
+          }
+
+          result.innerHTML = `${number * currentRates[id]}`;
+          id = 0;
+          return result;
+        };
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert(error.message);
+      });
   }
 };
 
